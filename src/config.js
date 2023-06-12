bd7f0f6351048b996f7ba11952332f8884b72091
naknamu <jodeldelvalle@gmail.com>
2023-06-12 13:45:17 +0800

    Added API url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
   apiUrl:
-    process.env.NODE_ENV === "development" ? "http://localhost:3000/api" : "",
+    process.env.NODE_ENV === "development" ? "http://localhost:3000/api" : "https://pinoytales-production.up.railway.app/api",
 
   userUrl:
     process.env.NODE_ENV === "development" ? "http://localhost:3000/user" : "",
